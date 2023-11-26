0e9f264eb8b13626aca8b1b851f109e4f41a8111
MatencaladaDenev <matencalada2002@gmail.com>
2023-11-25 23:56:33 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace PryRabbitColas1
+{
+
+
+    class PryRabbitColas1
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("TestTopic", ExchangeType.Topic);
+
+                string message = "Mensaje de notificación para notificacionesMail";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "TestTopic",
+                                     routingKey: "notificaciones",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine("Mensaje enviado a notificacionesMail: {0}", message);
+            }
+
+            Console.WriteLine("Presiona cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
